2bd28c60806fa6ac55b0f47d90c28b5a70424a86
Davi Carvalho <davi.carvalho@redemaisvoce.com.br>
2018-06-12 17:52:15 -0300

    alterações

--- introducao/src/settings.php
+++ introducao/src/settings.php
@@ -2,9 +2,11 @@
 return [
     'settings'=> [
         'displayErrorDetails'=>true,
+
         'renderer' => [
             'template_path'=>__DIR__.'/../template/'
         ],
+
         'logger' => [
             'name'=>'slim-app',
             'path'=>__DIR__.'/../logs/app.log',
